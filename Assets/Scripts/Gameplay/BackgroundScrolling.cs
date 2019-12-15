7f86ca7f6ad7c96a4f36b11c4b1866080b8afed9
vedangJavdekar <vedang.javdekar@gmail.com>
2019-12-16 00:19:40 +0530

    Fuel system + parallax background

---
+++ Assets/Scripts/Gameplay/BackgroundScrolling.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScrolling : MonoBehaviour
+{
+    public float speed;
+
+    public float endX;
+    public float startX;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        if(transform.position.x <= endX)
+        {
+            Vector3 pos = new Vector3(startX, transform.position.y,transform.position.z);
+            transform.position = pos;
+        }
+    }
+}
